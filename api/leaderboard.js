5af2a49b717d572fde342452df62c14daae6cfa3
Dekuusan <135011369+Dekuusan@users.noreply.github.com>
2025-11-18 16:03:20 +0700

    Refactor leaderboard handler to use path module

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -1,7 +1,18 @@
-export default function handler(req,res){
-  const fs=require('fs');
-  let db=[];
-  try{ db=JSON.parse(fs.readFileSync('db.json')); }catch{}
-  db.sort((a,b)=>b.score-a.score);
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'api', 'db.json');
+
+  let db = [];
+
+  try {
+    db = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+  } catch (e) {
+    db = [];
+  }
+
+  db.sort((a, b) => b.score - a.score);
+
   res.json(db);
 }
